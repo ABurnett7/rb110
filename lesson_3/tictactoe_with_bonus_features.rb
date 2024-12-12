62749ae657065dc49f8865360141fed55546e8f2
Andy Burnett <38389218+ABurnett7@users.noreply.github.com>
2024-12-12 16:22:25 -0700

    TTT bonus features

--- lesson_3/tictactoe_with_bonus_features.rb
+++ lesson_3/tictactoe_with_bonus_features.rb
@@ -4,6 +4,7 @@ COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
+MATCH_POINTS = 5
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -39,10 +40,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first.to_s
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
+    prompt "Choose a position to place a piece: #{joinor(empty_squares(brd))}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry that's not a valid choice"
@@ -50,8 +62,35 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+
+  # offense first
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+      break if square
+    end
+  end
+  # defense 
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd, PLAYER_MARKER)
+    break if square
+  end
+  
+  # just pick a square
+  if !square
+    square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
@@ -74,6 +113,9 @@ def detect_winner(brd)
   nil
 end
 
+computer_wins = 0
+player_wins = 0 
+
 loop do
   board = initialize_board
 
@@ -89,14 +131,27 @@ loop do
   display_board(board)
 
   if someone_won?(board)
+    case detect_winner(board)
+    when 'Computer' then computer_wins += 1
+    when 'Player' then player_wins += 1
+    end
     prompt "#{detect_winner(board)} won!"
+    prompt "First to 5 wins!"
+    prompt "Player: #{player_wins}.  Computer: #{computer_wins}"
+    sleep 1.5
   else
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  if (player_wins || computer_wins) == 5
+    if player_wins == MATCH_POINTS
+      prompt "You won!"
+      break
+    elsif computer_wins == MATCH_POINTS
+      prompt "Computer won.  Better luck next time"
+      break
+    end
+  end
 end
 
 prompt "Thanks for playing Tic Tac Toe!  Good bye!"
